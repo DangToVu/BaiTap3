304f78bad3811b45ada50591d3145ce153ab9631
DangToVu <vuminhdang811@gmail.com>
2024-06-19 09:14:12 +0700

    BaiTap3

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.square1}></View>
+      <View style={styles.square2}></View>
+      <View style={styles.square3}></View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  square1: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'lightblue',
+    margin: 10,
+  },
+  square2: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'darkgrey',
+    margin: 10,
+  },
+  square3: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'lime',
+    margin: 10,
   },
 });
+
+export default App;
